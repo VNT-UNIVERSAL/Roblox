f9f492b54dac613f297a075c4be1196700630f67
VNT-UNIVERSAL <vlr.ctcp@gmail.com>
2024-07-29 13:01:04 +0700

    Update 5034.lua

--- UILibrary/5034.lua
+++ UILibrary/5034.lua
@@ -1909,7 +1909,7 @@ ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable = true
-ImageButton.Image = "http://www.roblox.com/asset/?id="..(_G.Logo)
+ImageButton.Image = "http://www.roblox.com/asset/?id="..(tostring(_G.Logo))
 ImageButton.MouseButton1Down:connect(function()
 	game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
 	game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
