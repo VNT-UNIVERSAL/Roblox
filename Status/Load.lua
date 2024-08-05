4b160c5da770123f371a8373ecf2ecac45b60183
VNT-UNIVERSAL <vlr.ctcp@gmail.com>
2024-08-05 18:18:30 +0700

    Update Load.lua

--- Status/Load.lua
+++ Status/Load.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 142823291 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/mm2.lua'))()
    elseif game.PlaceId == 15148803832 then
-      loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/cdvn.lua')()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/cdvn.lua'))()
   else
     game.Players.LocalPlayer:Kick('Game Not Supported')
 end
