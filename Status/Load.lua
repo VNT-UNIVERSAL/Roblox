43a0731bcf82c9650ced6db7465d02a92dd84c80
VNT-UNIVERSAL <vlr.ctcp@gmail.com>
2024-08-08 20:34:34 +0700

    Update Load.lua

--- Status/Load.lua
+++ Status/Load.lua
@@ -1,6 +1,26 @@
 if game.PlaceId == 142823291 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/mm2.lua'))()
    elseif game.PlaceId == 15148803832 then
+   local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+        
+ScreenGui.Name = "VNT"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+UILock = true
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(221, 204, 204)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
+ImageButton.Size = UDim2.new(0, 50, 0, 50)
+ImageButton.Draggable = true
+ImageButton.Image = "http://www.roblox.com/asset/?id="
+ImageButton.MouseButton1Down:connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.RightControl,false,game)
+    game:GetService("VirtualInputManager"):SendKeyEvent(false,Enum.KeyCode.RightControl,true,game)
+end)
+UICorner.Parent = ImageButton
       loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/cdvn.lua'))()
       elseif game.PlaceId == 10260193230 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Roblox/main/Game/memesea.lua'))()
